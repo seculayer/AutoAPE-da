e18da3f077ca9abf7bfcdc8317bfdd71434f0b2b
yellowjin <yellowjin.dev@gmail.com>
2021-10-26 08:04:11 +0900

    init

---
+++ dataanalyzer/analyzer/TableDatasetMeta.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Author : Jin Kim
+# e-mail : jin.kim@seculayer.com
+# Powered by Seculayer © 2021 AI Service Model Team, R&D Center.
+from dataanalyzer.analyzer.DatasetMeta import DatasetMeta
+
+
+class TableDatasetMeta(DatasetMeta):
+    def __init__(self):
+        DatasetMeta.__init__(self)
+
--- dataanalyzer/dataloader/DataLoader.py
+++ dataanalyzer/dataloader/DataLoader.py
@@ -2,15 +2,19 @@
 # Author : Jin Kim
 # e-mail : jin.kim@seculayer.com
 # Powered by Seculayer © 2021 AI Service Model Team, R&D Center.
+from dataanalyzer.analyzer.TableDatasetMeta import TableDatasetMeta
 from dataanalyzer.common.Constants import Constants
 from dataanalyzer.info.DAJobInfo import DAJobInfo
 from dataanalyzer.util.sftp.PySFTPClient import PySFTPClient
+from dataanalyzer.analyzer.DatasetMeta import DatasetMeta
 
 
 class DataLoader(object):
     def __init__(self, job_info: DAJobInfo, sftp_client: PySFTPClient):
         self.job_info: DAJobInfo = job_info
         self.sftp_client: PySFTPClient = sftp_client
+
+        self.dataset_meta: DatasetMeta = DatasetMeta()
 
     def load(self):
         return {
@@ -19,6 +23,7 @@ class DataLoader(object):
 
     def load_table(self):
         f = self.sftp_client.open(self.job_info.get_filepath(), "r")
+        self.dataset_meta: TableDatasetMeta()
         while True:
             line = f.readline()
             print(line)
