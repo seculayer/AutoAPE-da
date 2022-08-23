5c74bfa756b546246ce143e0fdf638d3d3b9cc99
이원준 <wonjoon.lee@seculayer.com>
2022-08-23 13:42:24 +0900

    Poetry 적용 및 Dockerfile 빌드 변경

--- build-da.sh
+++ build-da.sh
@@ -3,8 +3,15 @@
 VERSION="1.0.0"
 MODULE_NAME="da"
 REGISTRY_URL="registry.seculayer.com:31500"
-SECRET_PATH="../access_token/token"
+GITCONFIG="../gitconfig"
+SSL_CERT="../slroot.crt"
+
+if [ -x "$(command -v podman)" ]; then
+  function docker { podman "$@"; }
+fi
 
 # docker build
-DOCKER_BUILDKIT=1 docker build --no-cache --secret id=token,src=$SECRET_PATH -t $REGISTRY_URL/ape/automl-$MODULE_NAME:$VERSION .
+DOCKER_BUILDKIT=1 docker build --secret id=gitconfig,src="$GITCONFIG" \
+  --secret id=cert,src="${SSL_CERT}" \
+  -t $REGISTRY_URL/ape/automl-$MODULE_NAME:$VERSION .
 docker push $REGISTRY_URL/ape/automl-$MODULE_NAME:$VERSION
--- da.sh
+++ da.sh
@@ -2,15 +2,21 @@
 
 APP_PATH=/eyeCloudAI/app/ape
 
-export PYTHONPATH=$PYTHONPATH:$APP_PATH/da/lib:$APP_PATH/da
-export PYTHONPATH=$PYTHONPATH:$APP_PATH/pycmmn/lib:$APP_PATH/pycmmn
-
 DATASET_ID=${1}
 WORKER_IDX=${3}
 
+if [ -x "$APP_PATH/da/.venv/bin/python3" ]
+then
+  PYTHON_BIN="$APP_PATH/da/.venv/bin/python3"
+else
+  PYTHON_BIN="$(command -v python3)"
+  export PYTHONPATH=$PYTHONPATH:$APP_PATH/da/lib:$APP_PATH/da
+  export PYTHONPATH=$PYTHONPATH:$APP_PATH/pycmmn/lib:$APP_PATH/pycmmn
+fi
+
 if [ "${2}" = "chief" ]
 then
-  /usr/local/bin/python3.7 -m dataanalyzer.DataAnalyzerChief ${DATASET_ID} ${WORKER_IDX}
+  $PYTHON_BIN -m dataanalyzer.DataAnalyzerChief ${DATASET_ID} ${WORKER_IDX}
 else
-  /usr/local/bin/python3.7 -m dataanalyzer.DataAnalyzerWorker ${DATASET_ID} ${WORKER_IDX}
+  $PYTHON_BIN -m dataanalyzer.DataAnalyzerWorker ${DATASET_ID} ${WORKER_IDX}
 fi
